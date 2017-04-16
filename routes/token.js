a65e7fd66be208e952a452f9bd3227d449d82497
GracieChristine <graciechristineli@gmail.com>
2017-04-16 15:33:07 -0600

    Completed the token routes' tests.

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,81 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+let express = require("express");
+let router = express.Router();
+let knex = require("../knex");
+let {
+      camelizeKeys,
+      decamelizeKeys
+    } = require("humps");
+let bcrypt = require("bcrypt");
+let boom = require("boom");
+let jwt = require("jsonwebtoken");
+let cookie = require("cookie-session");
 
-// eslint-disable-next-line new-cap
-const router = express.Router();
+//  GET
+router.get("/", (req, res, next) => {
+  if (!req.cookies.token) {
+    //  GET /token without token
+    res.status(200).send(false);
+  }
+  else {
+    //  GET /token with token
+    res.status(200).send(true);
+  }
+});
 
-// YOUR CODE HERE
+//  POST
+router.post("/", (req, res, next) => {
+  let email = req.body.email;
+  let password = req.body.password;
+
+  //  POST /token with no email
+  if (!email || email.trim() === "") {
+    return next(boom.create(400, "Email must not be blank"));
+  }
+
+  //  POST /token with no password
+  if (!password || password.trim() === "") {
+    return next(boom.create(400, "Password must not be blank"));
+  }
+
+  knex("users")
+    .where("email", email)
+    .then((users) => {
+      if (users.length > 0) {
+        //  POST /token
+        bcrypt.compare(password, users[0].hashed_password, (err, boolean) => {
+          if (boolean) {
+            let token = jwt.sign({
+              email: users[0].email,
+              password: users[0].hashed_password
+            }, "shhh");
+
+            res.cookie("token", token, {
+              httpOnly: true
+            });
+            delete users[0].hashed_password
+            res.send(camelizeKeys(users[0]))
+          }
+          else {
+            //  POST /token with bad password
+            next(boom.create(400, "Bad email or password"))
+          }
+        })
+      }
+      else {
+        //  POST /token with bad email
+        next(boom.create(400, "Bad email or password"))
+      }
+    })
+})
+
+//  DELETE
+router.delete("/", (req, res, session) => {
+  //  DELETE /token
+  res.clearCookie("token");
+  res.send(true);
+});
+
 
 module.exports = router;
