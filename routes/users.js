df7dc1dfe64623e5c86e8d648f7981a834c84718
GracieChristine <graciechristineli@gmail.com>
2017-04-15 20:07:08 -0600

    Completed Users routes bonus test.

--- routes/users.js
+++ routes/users.js
@@ -14,20 +14,37 @@ let boom = require("boom");
 router.post("/", (req, res, next) => {
   let newUser = req.body;
 
-  knex("users")
-    .returning(["id", "first_name", "last_name", "email"])
-    .insert({
-      first_name: newUser.firstName,
-      last_name: newUser.lastName,
-      email: newUser.email,
-      hashed_password: bcrypt.hashSync(newUser.password, 10)
-    })
-    .then((users) => {
-      res.send(camelizeKeys(users[0]))
-    })
-    .catch((error) => {
-      next(error);
-    });
+  if (!newUser.email || newUser.email.trim() === "") {
+    return next(boom.create(400, "Email must not be blank"));
+  }
+  else if (!newUser.password || newUser.password.trim() === "") {
+    return next(boom.create(400, "Password must be at least 8 characters long"));
+  }
+  else {
+    knex("users")
+      .where("email", newUser.email)
+      .then((existUser) => {
+        if (existUser.length > 0) {
+          return next(boom.create(400, "Email already exists"))
+        }
+        else {
+          knex("users")
+          .returning(["id", "first_name", "last_name", "email"])
+          .insert({
+            first_name: req.body.firstName,
+            last_name: req.body.lastName,
+            email: req.body.email,
+            hashed_password: bcrypt.hashSync(req.body.password, 8)
+          })
+          .then((users) => {
+            res.send(camelizeKeys(users[0]))
+          })
+          .catch((error) => {
+            next(error)
+          });
+        }
+      });
+  }
 });
 
 module.exports = router;
